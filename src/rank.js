25dd2a734d87304cb4fcb2fb1310fb87319dec70
Guo-zy <674402145@qq.com>
2020-08-28 19:03:09 +0800

    [homework] [Yancy] refactor : extract calculateResultByLengthFromHistory function

--- src/rank.js
+++ src/rank.js
@@ -1,7 +1,7 @@
 function voyageRisk(voyage) {
   let result = 1;
-  result += calculateResultByLengthFromVoyage(voyage , 4 , 2);
-  result += calculateResultByLengthFromVoyage(voyage , 8 , voyage.length - 8);
+  result += calculateResultByLengthFromVoyage(voyage, 4, 2);
+  result += calculateResultByLengthFromVoyage(voyage, 8, voyage.length - 8);
   result += calculateResultByZoneFromVoyage(voyage, 4)
   return Math.max(result, 0);
 }
@@ -12,7 +12,7 @@ function hasChina(history) {
 
 function captainHistoryRisk(voyage, history) {
   let result = 1;
-  result += calculateResultByLengthFromVoyage(voyage , 5 , 4);
+  result += calculateResultByLengthFromVoyage(voyage, 5, 4);
   result += history.filter(v => v.profit < 0).length;
   if (voyage.zone === 'china' && hasChina(history)) {
     result -= 2;
@@ -31,21 +31,21 @@ function calculateResultByLengthFromVoyage(voyage, tmp, count) {
   return voyage.length > tmp ? count : 0;
 }
 
+function calculateResultByLengthFromHistory(history, tmp, count) {
+  return history.length > tmp ? count : 0;
+}
+
 function voyageProfitFactor(voyage, history) {
   let result = 2;
   result += calculateResultByZoneFromVoyage(voyage, 1)
 
   if (voyage.zone === 'china' && hasChina(history)) {
     result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
+    result += calculateResultByLengthFromHistory(history, 10, 1);
     result += calculateResultByLengthFromVoyage(voyage, 12, 1);
     result += calculateResultByLengthFromVoyage(voyage, 18, -1);
   } else {
-    if (history.length > 8) {
-      result += 1;
-    }
+    result += calculateResultByLengthFromHistory(history, 8, 1);
     result += calculateResultByLengthFromVoyage(voyage, 14, -1);
 
   }
