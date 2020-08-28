d2ce720fe1ec214140e9999f2cf17192e27cf028
Guo-zy <674402145@qq.com>
2020-08-28 17:43:03 +0800

    [homework] [Yancy] refactor : extract calculateResultByZoneFromVoyage function

--- src/rank.js
+++ src/rank.js
@@ -31,14 +31,14 @@ function captainHistoryRisk (voyage, history) {
   return Math.max(result, 0);
 }
 
+function calculateResultByZoneFromVoyage(voyage , zoneName){
+    return voyage.zone === zoneName ? 1 : 0; 
+}
+
 function voyageProfitFactor (voyage, history) {
   let result = 2;
-  if (voyage.zone === 'china') {
-    result += 1;
-  }
-  if (voyage.zone === 'east-indies') {
-    result += 1;
-  }
+  result += calculateResultByZoneFromVoyage(voyage , 'china');
+  result += calculateResultByZoneFromVoyage(voyage , 'east-indies')
   if (voyage.zone === 'china' && hasChina(history)) {
     result += 3;
     if (history.length > 10) {
@@ -74,28 +74,4 @@ function rating (voyage, history) {
   }
 }
 
-module.exports = {
-};
-
-const voyage = {
-  zone: 'west-indies',
-  length: 10,
-};
-const history = [
-  {
-    zone: 'east-indies',
-    profit: 5,
-  },{
-    zone: 'west-indies',
-    profit: 15,
-  },{
-    zone: 'china',
-    profit: -2,
-  },
-  {
-    zone: 'west-africa',
-    profit: 7,
-  },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
+module.exports = rating;
